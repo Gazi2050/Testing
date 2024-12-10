dda67ade94cf0f14cf445ac9c5533ab2bc3b73d2
Gazi2050 <gazi45344@gmail.com>
2024-12-11 01:56:32 +0600

    update

---
+++ react-testing-starter/tests/components/UserAccount.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import { User } from '../../src/entities'
+import UserAccount from '../../src/components/UserAccount';
+
+describe('userAccount', () => {
+    it('should render user name', () => {
+        const user: User = { id: 1, name: 'Araf' };
+        render(<UserAccount user={user} />);
+        expect(screen.getByText(user.name)).toBeInTheDocument();
+    });
+    it('should not  render edit button if user is not admin', () => {
+        const user: User = { id: 1, name: 'Araf', isAdmin: true };
+        render(<UserAccount user={user} />);
+        const button = screen.getByRole('button');
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/edit/i);
+    })
+})
